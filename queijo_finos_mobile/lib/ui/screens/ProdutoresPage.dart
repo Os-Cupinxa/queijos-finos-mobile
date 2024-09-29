dc63155ec75ccd4e0e7b74fe92f272d0b85db914
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-09-28 22:16:31 -0300

    feat: DetalhesPage e ProdutoresPage

--- queijo_finos_mobile/lib/ui/screens/ProdutoresPage.dart
+++ queijo_finos_mobile/lib/ui/screens/ProdutoresPage.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
-
-class ProprietyDTO {
-  final String name;
-  final String onwer;
-  final String city;
-  final String state;
-  final Status status;
-
-  ProprietyDTO({
-    required this.name,
-    required this.onwer,
-    required this.city,
-    required this.state,
-    required this.status,
-  });
-}
-
-enum Status { PRODUZINDO, PARADO }
+import 'package:queijo_finos_mobile/models/ActiveContracts.dart';
+import 'package:queijo_finos_mobile/ui/screens/details/DetalhesPage.dart';
+import '../../models/ProprietyDTO.dart';
 
 class ProdutoresPage extends StatelessWidget {
   final List<ProprietyDTO> _proprietes = [
@@ -26,6 +11,19 @@ class ProdutoresPage extends StatelessWidget {
       city: 'São Paulo',
       state: 'SP',
       status: Status.PRODUZINDO,
+      contractsActive: [
+        ActiveContracts(
+          tecnologia: 'Tecnologia Gorgonzola',
+          dataInicio: DateTime(2023, 1, 10),
+          dataFim: DateTime(2024, 1, 10),
+        ),
+        ActiveContracts(
+            tecnologia: "Tecnologia Parmesao",
+            dataInicio: DateTime(2023, 1, 10),
+            dataFim: DateTime(2024, 1, 10))
+      ],
+      latitude: '-22.9732303',
+      longitude: '-43.2032649',
     ),
     ProprietyDTO(
       name: 'Fazenda São José',
@@ -33,6 +31,9 @@ class ProdutoresPage extends StatelessWidget {
       city: 'São Paulo',
       state: 'SP',
       status: Status.PRODUZINDO,
+      contractsActive: [],
+      latitude: '-23.5505199',
+      longitude: '-46.6333094',
     ),
     ProprietyDTO(
       name: 'Fazenda Esperança',
@@ -40,9 +41,17 @@ class ProdutoresPage extends StatelessWidget {
       city: 'São Paulo',
       state: 'SP',
       status: Status.PARADO,
+      contractsActive: [
+        ActiveContracts(
+          tecnologia: 'Teconologia Gorgonzola',
+          dataInicio: DateTime(2022, 5, 1),
+          dataFim: DateTime(2023, 5, 1),
+        ),
+      ],
+      latitude: '-23.5505199',
+      longitude: '-46.6333094',
     ),
   ];
-
   @override
   final TextEditingController _searchController = TextEditingController();
 
@@ -56,6 +65,7 @@ class ProdutoresPage extends StatelessWidget {
     }
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
@@ -78,129 +88,121 @@ class ProdutoresPage extends StatelessWidget {
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: _proprietes.length,
-                    itemBuilder: (context, index) {
-                      final item = _proprietes[index];
+              child: ListView.builder(
+                itemCount: _proprietes.length,
+                itemBuilder: (context, index) {
+                  final item = _proprietes[index];
 
-                      return Padding(
-                        padding: const EdgeInsets.only(
-                            right: 4.0, left: 4.0, bottom: 16.0),
-                        child: Container(
-                          width: double.infinity,
-                          height: 120,
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFF4F2F2),
-                            borderRadius: BorderRadius.circular(8.0),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.2),
-                                spreadRadius: 2,
-                                blurRadius: 2,
-                                offset: const Offset(0, 3),
-                              ),
-                            ],
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                        right: 4.0, left: 4.0, bottom: 16.0),
+                    child: Container(
+                      width: double.infinity,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFF4F2F2),
+                        borderRadius: BorderRadius.circular(8.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 2,
+                            blurRadius: 2,
+                            offset: const Offset(0, 3),
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
+                                Text(item.name,
+                                    style: const TextStyle(
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.bold)),
                                 Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Row(
-                                      children: [
-                                        Text(item.name,
-                                            style: const TextStyle(
-                                                fontSize: 22.0,
-                                                fontWeight: FontWeight.bold)),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        Text(item.city + ' - ' + item.state,
-                                            style: const TextStyle(
-                                                fontSize: 16.0,
-                                                color: Color(0xFF8C8C8C))),
-                                        const SizedBox(width: 8),
-                                        const Icon(
-                                          Icons.location_on,
-                                          color: Color(0xFF8C8C8C),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    const Icon(Icons.person_pin,
-                                        color: Colors
-                                            .grey), 
-                                    const SizedBox(width: 8),
-                                    Text(item.onwer,
+                                    Text(item.city + ' - ' + item.state,
                                         style: const TextStyle(
                                             fontSize: 16.0,
                                             color: Color(0xFF8C8C8C))),
-                                  ],
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Icon(
-                                            item.status == Status.PRODUZINDO
-                                                ? Icons.play_arrow
-                                                : Icons.pause,
-                                            color: Color(0xFF8C8C8C)),
-                                        SizedBox(width: 8),
-                                        Text(
-                                          item.status == Status.PRODUZINDO
-                                              ? 'Produzindo'
-                                              : 'Parado',
-                                          style: const TextStyle(
-                                              fontSize: 16.0,
-                                              color: Color(0xFF8C8C8C)),
-                                        ),
-                                      ],
-                                    ),
-                                    OutlinedButton.icon(
-                                      style: OutlinedButton.styleFrom(
-                                          side: const BorderSide(
-                                        color: Color(0xFF79747E),
-                                        width: 2.0,
-                                      )),
-                                      icon: const Icon(
-                                          Icons.arrow_right_outlined,
-                                          color: Color(0xFF79747E),
-                                          size: 24.0),
-                                      onPressed: () {
-                                        print('Ver detalhes do produtor');
-                                      },
-                                      label: const Text(
-                                        'Ver Mais',
-                                        style: TextStyle(
-                                            color: Color(0xFF79747E),
-                                            fontWeight: FontWeight.bold),
-                                      ),
+                                    const SizedBox(width: 8),
+                                    const Icon(
+                                      Icons.location_on,
+                                      color: Color(0xFF8C8C8C),
                                     ),
                                   ],
                                 ),
                               ],
                             ),
-                          ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                const Icon(Icons.person_pin,
+                                    color: Colors.grey),
+                                const SizedBox(width: 8),
+                                Text(item.onwer,
+                                    style: const TextStyle(
+                                        fontSize: 16.0,
+                                        color: Color(0xFF8C8C8C))),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                        item.status == Status.PRODUZINDO
+                                            ? Icons.play_arrow
+                                            : Icons.pause,
+                                        color: Color(0xFF8C8C8C)),
+                                    const SizedBox(width: 8),
+                                    Text(
+                                      item.status == Status.PRODUZINDO
+                                          ? 'Produzindo'
+                                          : 'Parado',
+                                      style: const TextStyle(
+                                          fontSize: 16.0,
+                                          color: Color(0xFF8C8C8C)),
+                                    ),
+                                  ],
+                                ),
+                                OutlinedButton.icon(
+                                  style: OutlinedButton.styleFrom(
+                                      side: const BorderSide(
+                                    color: Color(0xFF79747E),
+                                    width: 2.0,
+                                  )),
+                                  icon: const Icon(Icons.arrow_right_outlined,
+                                      color: Color(0xFF79747E), size: 24.0),
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            DetalhesPage(item: item),
+                                      ),
+                                    );
+                                  },
+                                  label: const Text(
+                                    'Ver Mais',
+                                    style: TextStyle(
+                                        color: Color(0xFF79747E),
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                      );
-                    },
-                  )
-                ],
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
           ),
---
+++ queijo_finos_mobile/lib/ui/screens/details/DetalhesPage.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart'; // Importa o pacote
+import '../../../models/ProprietyDTO.dart';
+
+class DetalhesPage extends StatelessWidget {
+  final ProprietyDTO item;
+
+  const DetalhesPage({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+                child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(200),
+                      border:
+                          Border.all(color: const Color(0xFF8C8C8C), width: 8),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Icon(Icons.person,
+                          size: 150, color: Color(0xFF8C8C8C)),
+                    ))),
+            const SizedBox(height: 20),
+            Text(item.name,
+                style:
+                    const TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+            Text(item.onwer,
+                style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey)),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(right: 8.0),
+                  child: Icon(Icons.location_on, color: Colors.grey, size: 28),
+                ),
+                Text('${item.city} - ${item.state}',
+                    style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w500)),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                OutlinedButton.icon(
+                  onPressed: () => _openMap(item.latitude, item.longitude),
+                  icon: const Icon(Icons.location_on, color: Color(0xFF0D2434)),
+                  label: const Text(
+                    'Localização no mapa',
+                    style: TextStyle(
+                      color: Color(0xFF0D2434),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Text(item.status == Status.PRODUZINDO ? 'Produzindo' : 'Parado',
+                style: const TextStyle(fontSize: 20)),
+            const SizedBox(height: 16),
+            const Text("Contratos Ativos:",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+            const SizedBox(height: 16),
+            Expanded(
+              child: ListView.builder(
+                itemCount: item.contractsActive.length,
+                itemBuilder: (context, index) {
+                  final contract = item.contractsActive[index];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 16),
+                        child: RichText(
+                          text: TextSpan(
+                            style: const TextStyle(
+                                fontSize: 18, color: Colors.black),
+                            children: [
+                              const TextSpan(
+                                  text: 'Contrato de ',
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.normal)),
+                              TextSpan(
+                                  text: '${contract.tecnologia}',
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        ),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 14),
+                          children: [
+                            const TextSpan(
+                                text: 'Contrato emitido dia ',
+                                style:
+                                    TextStyle(fontWeight: FontWeight.normal)),
+                            TextSpan(
+                                text: '${dateFormatted(contract.dataInicio)}',
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold)),
+                            const TextSpan(
+                                text:
+                                    ' com data de vencimento prevista para dia ',
+                                style:
+                                    TextStyle(fontWeight: FontWeight.normal)),
+                            TextSpan(
+                                text: '${dateFormatted(contract.dataFim)}',
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ),
+                      const Divider(),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _openMap(String latitude, String longitude) async {
+    final Uri url = Uri.parse(
+        'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
+
+String dateFormatted(DateTime date) {
+  return '${date.day}/${date.month}/${date.year}';
+}
